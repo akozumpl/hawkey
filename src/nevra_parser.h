6258c01bd64129d1a8ecb3fe6aa3ccf8986cf7d5
Jan Silhan <jsilhan@redhat.com>
2013-11-15 16:16:21 +0100

    added hy_parse_nevra function

---
+++ src/nevra_parser.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012-2013 Red Hat, Inc.
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef HY_NEVRA_PARSER
+#define HY_NEVRA_PARSER
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int hy_parse_nevra(const char *nevra, char **name, char **evr, int *cmp_type);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HY_NEVRA_PARSER */
